aa0ab53eeea4dd798f2b703055c49d4626ccde52
Shaikh Faiyaz Karim <190041116faiyaz@gmail.com>
2023-04-04 02:25:59 +0600

    Added more solutions

---
+++ Brute Force/Two_Sum.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector <int> ans;
+        int flag = 0;
+
+        for(int i=0; i<nums.size(); i++){
+            for(int j=0; j<nums.size(); j++){
+                if(i==j)
+                {
+                    continue;
+                }
+                if(nums[i]+nums[j] == target){
+                    ans.push_back(i);
+                    ans.push_back(j);
+                    flag = 1;
+                    break;
+                }
+            }
+            if(flag){
+                break;
+            }
+        }
+        return ans;
+    }
+};
